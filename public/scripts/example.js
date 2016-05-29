fb40ec1358445ce6eefabd02ed3d99698cb1ca62
Leandro Barbosa <leandrobar93@gmail.com>
2016-05-29 15:59:00 -0300

    parts of tutorial that i get data from api

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,17 +1,87 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    var CommentBox = React.createClass({
-        render: function() {
-            return (
-                <div className="commentBox">
-                Hello, world! I am a CommentBox.
-                    </div>
-            );
-        }
-    });
+	var data_bk = [
+		{id: 1, author: "Pete Hunt", text: "This is one comment"},
+		{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+	];
+
+	var CommentBox = React.createClass({
+		getInitialState: function() {
+			return {data: []};
+		},
+		componentDidMount: function() {
+			$.ajax({
+				url: this.props.url,
+				dataType: 'json',
+				cache: false,
+				success: function(data) {
+					this.setState({data: data});
+				}.bind(this),
+				error: function(xhr, status, err) {
+					console.error(this.props.url, status, err.toString());
+					this.setState({data: data_bk});
+				}.bind(this)
+			});
+		},
+		render: function() {
+			return (
+				<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList data={this.state.data} />
+				<CommentForm />
+				</div>
+			);
+		}
+	});
+
+	var CommentList = React.createClass({
+		render: function() {
+			var commentNodes = this.props.data.map(function(comment) {
+				return (
+					<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+					</Comment>
+				);
+			});
+			return (
+				<div className="commentList">
+				{commentNodes}
+				</div>
+			);
+		}
+	});
+
+	var CommentForm = React.createClass({
+		render: function () {
+			return (
+				<div className="commentForm">
+					Hello, world I am a CommentForm.
+				</div>
+			);
+		}
+	});
+
+	var Comment = React.createClass({
+		rawMarkup: function() {
+			var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+			return { __html: rawMarkup };
+		},
+
+		render: function() {
+			return (
+				<div className="comment">
+				<h2 className="commentAuthor">
+				{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+				</div>
+			);
+		}
+	});
+
     ReactDOM.render(
-        <CommentBox />,
+		<CommentBox url="/api/comments" />,
         document.getElementById('content')
     );
 
